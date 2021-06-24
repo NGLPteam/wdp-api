6177000de9fdeae29288772d4bdbb03e3b7089f0
Alexa Grey <alexa@castironcoding.com>
2021-06-23 17:05:01 -0700

    [C] Add helper to grant a role to all communities

---
+++ app/operations/access/grant_all_communities.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Access
+  class GrantAllCommunities
+    include WDPAPI::Deps[grant: "access.grant"]
+    include Dry::Monads[:result, :do]
+
+    # @param [AccessGrantSubject] subject
+    def call(subject, role:)
+      Community.find_each do |community|
+        yield grant.call role, on: community, to: subject
+      end
+    end
+  end
+end
