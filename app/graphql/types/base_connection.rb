94a74ca3f159a5a4ea40224face7e63525ec73f3
Alexa Grey <alexa@castironcoding.com>
2021-07-08 10:41:20 -0700

    [F] Create interface for pageInfo
    Resolves WDP-99

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -5,6 +5,8 @@ module Types
     # add `nodes` and `pageInfo` fields, as well as `edge_type(...)` and `node_nullable(...)` overrides
     include GraphQL::Types::Relay::ConnectionBehaviors
 
+    implements Types::PaginatedType
+
     # Override built-in pageInfo field type with our type, supporting page-based pagination
     get_field("pageInfo").type = GraphQL::Schema::Member::BuildType.parse_type(Types::PageInfoType, null: false)
   end
