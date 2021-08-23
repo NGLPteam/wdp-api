63714afef6c40a14f312fa1dcbd521987b7a4bce
Alexa Grey <alexa@castironcoding.com>
2021-08-23 13:33:15 -0700

    [C] Improve User GraphQL type definition

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,22 +4,31 @@ module Types
   class UserType < Types::AbstractModel
     implements Types::ExposesPermissionsType
 
-    description "A user"
+    description "A known or anonymous user in the system. Registration and management is primarily handled through the WDP Keycloak instance."
 
-    field :anonymous, Boolean, null: false, method: :anonymous?
-    field :email_verified, Boolean, null: false
-    field :email, String, null: true
-    field :username, String, null: true
-    field :name, String, null: true
+    field :anonymous, Boolean, null: false, method: :anonymous?,
+      description: "Is this an anonymous / unauthenticated user?"
 
+    field :email_verified, Boolean, null: false,
+      description: "Has this user's email been verified to work through Keycloak?"
 
-    field :communities, resolver: Resolvers::AccessibleCommunityResolver
-    field :collections, resolver: Resolvers::AccessibleCollectionResolver
-    field :items, resolver: Resolvers::AccessibleItemResolver
+    field :email, String, null: true,
+      description: "A user's email. Depending on the upstream provider, this may not be set."
+    field :username, String, null: true,
+      description: "A unique username for the user. Depending on the upstream provider, this may not be set."
+    field :name, String, null: true,
+      description: "The user's full provided name. Depending on the upstream provider, this may not be set."
+
     field :global_admin, Boolean, null: false,
       description: "Does this user have access to administer the entire instance?",
       method: :has_global_admin_access?
 
+    field :communities, resolver: Resolvers::AccessibleCommunityResolver,
+      description: "Query the communities this user has access to"
+    field :collections, resolver: Resolvers::AccessibleCollectionResolver,
+      description: "Query the collections this user has access to"
+    field :items, resolver: Resolvers::AccessibleItemResolver,
+      description: "Query the items this user has access to"
 
     field :upload_access, Boolean, null: false,
       description: "Can this user upload anything at all?",
--- app/services/concerns/monadic_find.rb
+++ app/services/concerns/monadic_find.rb
@@ -4,7 +4,7 @@ module MonadicFind
   extend ActiveSupport::Concern
 
   def monadic_find(model_klass, *primary_key)
-    model = model_klass.find *primary_key
+    model = model_klass.find(*primary_key)
   rescue ActiveRecord::RecordNotFound => e
     Dry::Monads::Result::Failure[:record_not_found, e.message]
   else
