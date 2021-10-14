f111ea5696415960e6e0d4c0557881b1d2e99b16
Alexa Grey <alexa@castironcoding.com>
2021-10-13 19:51:14 -0700

    [C] Add test service for making community managers

---
+++ app/operations/testing/make_community_manager.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Testing
+  class MakeCommunityManager
+    include Dry::Monads[:do, :result]
+    include WDPAPI::Deps[grant_access: "access.grant"]
+    include MonadicPersistence
+
+    def call(user)
+      user.global_access_control_list.communities = {
+        manage_access: true,
+        read: true,
+        create: true,
+        update: true,
+        delete: true,
+        assets: {
+          read: true,
+          create: true,
+          update: true,
+          delete: true
+        }
+      }
+
+      yield monadic_save user
+
+      manager = Role.fetch "Manager"
+
+      Community.find_each do |community|
+        yield grant_access.call manager, on: community, to: user
+      end
+
+      Success true
+    end
+  end
+end
