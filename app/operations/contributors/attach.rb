fd696b842192cffc0a6926c25f6cbf2dbe44ec29
Alexa Grey <alexa@castironcoding.com>
2021-10-28 02:02:05 -0700

    [B] Correct contributors.attach action

--- app/operations/contributors/attach.rb
+++ app/operations/contributors/attach.rb
@@ -3,16 +3,21 @@
 module Contributors
   class Attach
     include Dry::Monads[:result, :do]
-    include WDPAPI::Deps[attach_collection: "contributors.attach_collection", attach_item: "contributors.attach_item"]
+    include WDPAPI::Deps[
+      attach_collection: "contributors.attach_collection",
+      attach_item: "contributors.attach_item",
+    ]
 
     def call(contributor, contributable)
       case contributable
       when Collection
-        attach_collection(contributor, contributable)
+        attach_collection.call(contributor, contributable)
       when Item
-        attach_item(contributor, contributable)
+        attach_item.call(contributor, contributable)
       else
+        # :nocov:
         Failure[:invalid_contributable, "#{contributable.inspect} is not contributable"]
+        # :nocov:
       end
     end
   end
