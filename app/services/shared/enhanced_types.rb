e718932352442953caa291b656068651c22d578f
Alexa Grey <alexa@castironcoding.com>
2021-12-01 21:45:01 -0800

    [C] Add enhanced types

---
+++ app/services/shared/enhanced_types.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Shared
+  module EnhancedTypes
+    extend ActiveSupport::Concern
+
+    def ClassOrSubclass(kls)
+      is_klass = ::Dry::Types["class"].constrained(eql: kls)
+
+      is_klass | Implements(kls)
+    end
+
+    def Implements(mod)
+      ::Dry::Types["class"].constrained(lt: mod)
+    end
+
+    alias Inherits Implements
+
+    def InstanceOrClass(mod)
+      instance = ::Dry::Types::Nominal.new(mod).constrained(type: mod)
+      klass = ClassOrSubclass(mod)
+
+      instance | klass
+    end
+
+    # @param [<Object>] values
+    # @param [Object, nil] fallback (will default to first value)
+    # @param ["coercible.string", "coercible.symbol"] type
+    # @return [Dry::Types::Type]
+    def enum_with_fallback(*values, type: "coercible.string", fallback: nil)
+      values.flatten!
+
+      fallback = fallback.presence_in(values) || values.first
+
+      Dry::Types[type].default(fallback).enum(*values).fallback(fallback)
+    end
+
+    # @param [<String>] values
+    # @param [String, nil] fallback (will default to first value)
+    # @return [Dry::Types::Type]
+    def string_enum(*values, fallback: nil)
+      enum_with_fallback(*values, type: "coercible.string", fallback: fallback)
+    end
+  end
+end
