b5d9dae31e6971b3ee78faf46f83bb40b3d8c2d2
Alexa Grey <alexa@castironcoding.com>
2021-12-23 09:10:43 -0800

    [C] Ensure semantic versions serialize as strings

--- lib/patches/support_semantic_version.rb
+++ lib/patches/support_semantic_version.rb
@@ -13,6 +13,44 @@ module Patches
       m.register_type "semantic_version", GlobalTypes::SemanticVersion.new
     end
   end
+
+  # An extension for StoreModel that makes sure to serialize attributes with
+  # their custom serialization logic rather than relying on `#as_json`.
+  module SerializeSpecialAttributesProperly
+    def as_json(options = {})
+      super.merge(properly_serialized_attributes)
+    end
+
+    def properly_serialized_attributes
+      self.class.special_attribute_types.each_with_object({}) do |(key, value), attrs|
+        attrs[key] = value.serialize_for_store_model attributes.fetch(key)
+      end
+    end
+
+    module ClassMethods
+      def special_attribute_types
+        attribute_types.select do |_, value|
+          value.respond_to?(:serialize_for_store_model)
+        end
+      end
+    end
+
+    module ClassMethodsInclusion
+      def included(base)
+        super if defined?(super)
+
+        base.extend Patches::SerializeSpecialAttributesProperly::ClassMethods
+      end
+    end
+
+    class << self
+      def prepended(base)
+        base.singleton_class.prepend ClassMethodsInclusion
+      end
+    end
+  end
 end
 
 ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend Patches::SupportSemanticVersion
+
+StoreModel::Model.prepend Patches::SerializeSpecialAttributesProperly
