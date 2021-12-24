e3f95e5ef46d63a7bc54860d44f21e601aafaf1c
Alexa Grey <alexa@castironcoding.com>
2021-12-23 22:36:44 -0800

    [C] Add support for composing GraphQL interfaces

--- app/graphql/types/base_interface.rb
+++ app/graphql/types/base_interface.rb
@@ -11,10 +11,43 @@ module Types
 
     field_class Types::BaseField
 
+    class ComposedInterface < Module
+      def initialize(interface:)
+        @interface = interface
+
+        # :nocov:
+
+        raise TypeError, "must be a GraphQL interface: #{interface}" unless interface.include? GraphQL::Schema::Interface
+
+        # :nocov:
+
+        define_method(:included) do |child_class|
+          super(child_class) if defined?(super)
+
+          child_class.implements interface if child_class < GraphQL::Schema::Object
+        end
+      end
+
+      def inspect
+        "Types::BaseInterface::ComposedModule(#{@interface.name})"
+      end
+    end
+
+    module ClassMethods
+      def implements(interface)
+        include interface
+
+        mod = ComposedInterface.new interface: interface
+
+        singleton_class.prepend mod
+      end
+    end
+
     class << self
       def included(base)
         super if defined?(super)
 
+        base.extend Types::BaseInterface::ClassMethods if base.kind_of?(Module)
         base.extend Graphql::ImageAttachmentSupport::ClassMethods
       end
     end
