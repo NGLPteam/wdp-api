505599f13974e1b9a5259c295648c4a4fb9ff24b
Alexa Grey <alexa@castironcoding.com>
2022-01-10 11:02:18 -0800

    [C] Add missing dates to create/update inputs
    * Remove previously-deprecated `published_on` input

--- app/graphql/mutations/shared/hierarchical_entity_arguments.rb
+++ app/graphql/mutations/shared/hierarchical_entity_arguments.rb
@@ -8,22 +8,34 @@ module Mutations
       include Mutations::Shared::EntityArguments
 
       included do
+        argument :accessioned, Types::VariablePrecisionDateInputType, required: false,
+          description: "The date this entity was added to its parent",
+          attribute: true
+
+        argument :available, Types::VariablePrecisionDateInputType, required: false,
+          description: "The date this entity was made available",
+          attribute: true
+
+        argument :issued, Types::VariablePrecisionDateInputType, required: false,
+          description: "The date this entity was issued",
+          attribute: true
+
+        argument :published, Types::VariablePrecisionDateInputType, required: false,
+          description: "The date this entity was published",
+          attribute: true
+
         argument :summary, String, required: false,
           description: "A brief description of the entity's contents.",
           attribute: true
-        argument :published, Types::VariablePrecisionDateInputType, required: false,
-          description: "The date the entity was published",
-          attribute: true
-        argument :published_on, GraphQL::Types::ISO8601Date, required: false,
-          description: "The date the entity was published",
-          deprecation_reason: "Use the variable 'published' input instead",
-          attribute: true
+
         argument :visibility, Types::EntityVisibilityType, required: true,
           description: "What level of visibility the entity has",
           attribute: true
+
         argument :visible_after_at, GraphQL::Types::ISO8601DateTime, required: false,
           description: "If present, this is the timestamp an entity is visible after",
           attribute: true
+
         argument :visible_until_at, GraphQL::Types::ISO8601DateTime, required: false,
           description: "If present, this is the timestamp an entity is visible until",
           attribute: true
