eb08744bf14db9fad2d59fb290f0b064f8f9828f
Alexa Grey <alexa@castironcoding.com>
2022-03-05 05:14:01 -0800

    [C] Add UCM OAI flow

---
+++ app/operations/test_oai/flows/merced.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module TestOAI
+  module Flows
+    class Merced
+      include WDPAPI::Deps[
+        workflow: "test_oai.workflow"
+      ]
+
+      def call
+        args = {
+          community_options: {
+            title: "UC Merced"
+          },
+          source_options: {
+            name: "UC Merced OAI Test",
+            protocol: "oai",
+            metadata_format: "mets",
+            base_url: "https://dspace-pilot.escholarship.org/server/oai/request",
+          },
+          on_community: true
+        }
+
+        workflow.call args
+      end
+    end
+  end
+end
