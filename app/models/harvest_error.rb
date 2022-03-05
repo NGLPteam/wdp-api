5bb749d25bf65978aeb460fb4fe063b7d88cb6b1
Alexa Grey <alexa@castironcoding.com>
2022-03-05 05:00:43 -0800

    [C] Improve HarvestError introspection

--- app/models/harvest_error.rb
+++ app/models/harvest_error.rb
@@ -4,14 +4,26 @@
 class HarvestError < ApplicationRecord
   belongs_to :source, polymorphic: true
 
+  UNKNOWN_COLLECTIONS = "existing collection with identifier"
+
+  scope :by_code, ->(code) { where(code: code) }
   scope :by_source_type, ->(type) { where source_type: type }
   scope :harvest_entities, -> { by_source_type "HarvestEntity" }
   scope :harvest_records, -> { by_source_type "HarvestRecord" }
   scope :latest_attempt, -> { build_latest_attempt_scope }
+  scope :message_contains, ->(needle) { where_contains(message: needle) }
+  scope :sans_message, ->(needle) { where.not(id: message_contains(needle)) }
+  scope :unknown_collections, -> { message_contains(UNKNOWN_COLLECTIONS) }
+  scope :sans_unknown_collections, -> { sans_message UNKNOWN_COLLECTIONS }
+  scope :with_invalid_parentage, -> { where(arel_json_get_path_as_text(:metadata, "reason", 0).eq("invalid_parentage")) }
 
   validates :code, :message, presence: true
 
   class << self
+    def latest_message(needle)
+      preload(:source).latest_attempt.message_contains(needle)
+    end
+
     # @api private
     # @return [ActiveRecord::Relation<HarvestError>]
     def build_latest_attempt_scope
