dc28cff80add6cef38447aa5e705f764231aa6c8
Alexa Grey <alexa@castironcoding.com>
2022-04-05 16:29:11 -0700

    [C] Prune obsolete harvest entities during extraction

--- app/operations/harvesting/entities/with_assigner.rb
+++ app/operations/harvesting/entities/with_assigner.rb
@@ -4,6 +4,7 @@ module Harvesting
   module Entities
     class WithAssigner
       include MonadicPersistence
+      include Dry::Effects.State(:extracted_entity_ids)
 
       # @param [HarvestEntity] harvest_entity
       # @yield [assigner] Yield an assignment to a specific entity
@@ -17,7 +18,11 @@ module Harvesting
 
         harvest_entity.assign_attributes assigner.to_attributes
 
-        monadic_save harvest_entity
+        monadic_save(harvest_entity).tap do |result|
+          result.bind do |record|
+            extracted_entity_ids << record.id
+          end
+        end
       end
     end
   end
