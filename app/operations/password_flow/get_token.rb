a0ade7a040f306d46c1fbd1349dfdf947067a4b5
Alexa Grey <alexa@castironcoding.com>
2022-04-05 18:15:47 -0700

    [C] Improve test client for password token exchange

--- app/operations/password_flow/get_token.rb
+++ app/operations/password_flow/get_token.rb
@@ -7,12 +7,41 @@ module PasswordFlow
 
     # @param [String] username
     # @param [String] password
+    # @param [:admin, nil] client_id
     # @return [OpenIDConnect::AccessToken]
-    def call(username, password)
-      client = build_client.call
+    def call(username, password, client_id: nil, via_admin: false)
+      if via_admin
+        admin_client = client_from client_id: :admin
 
-      client.resource_owner_credentials = [username, password]
+        admin_client.resource_owner_credentials = [username, password]
 
+        admin_token = token_from admin_client
+
+        exchange_client = client_from client_id: nil
+
+        exchange_client.subject_token = admin_token.access_token
+
+        token_from exchange_client
+      else
+        client = client_from client_id: client_id
+
+        client.resource_owner_credentials = [username, password]
+
+        token_from client
+      end
+    end
+
+    private
+
+    def client_from(client_id: nil, via_admin: false)
+      options = {}
+
+      options[:client_id] = via_admin ? :admin : client_id
+
+      build_client.call options
+    end
+
+    def token_from(client)
       client.access_token! scope: "openid profile email"
     end
   end
