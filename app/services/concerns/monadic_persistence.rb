f5b969bebbd43ddb29bc66be37c78183c20798ea
Alexa Grey <alexa@castironcoding.com>
2022-05-03 09:40:22 -0700

    [C] Better expose model errors

--- app/services/concerns/monadic_persistence.rb
+++ app/services/concerns/monadic_persistence.rb
@@ -3,11 +3,14 @@
 module MonadicPersistence
   extend ActiveSupport::Concern
 
+  # @param [ApplicationRecord] model
+  # @return [Dry::Monads::Success(ApplicationRecord)]
+  # @return [Dry::Monads::Failure(:invalid, ApplicationRecord, <String>)]
   def monadic_save(model)
     if model.save
       Dry::Monads.Success(model)
     else
-      Dry::Monads::Result::Failure[:invalid, model]
+      Dry::Monads::Result::Failure[:invalid, model, model.errors.full_messages.to_a]
     end
   end
 end
