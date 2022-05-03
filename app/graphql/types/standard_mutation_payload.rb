64ae473dae8e64b212b197765c835a7d62069274
Alexa Grey <alexa@castironcoding.com>
2022-05-03 09:40:43 -0700

    [C] Add deprecation note about mutation `errors`

--- app/graphql/types/standard_mutation_payload.rb
+++ app/graphql/types/standard_mutation_payload.rb
@@ -6,14 +6,14 @@ module Types
 
     description "Most mutations implement this interface in their payload in order to offer a standardize response value"
 
-    field :errors, [Types::UserError, { null: false }], null: false
-
     field :attribute_errors, [Types::MutationAttributeErrorType, { null: false }], null: false
 
     field :global_errors, [Types::MutationGlobalErrorType, { null: false }], null: false
 
     field :halt_code, String, null: true,
       description: "Not presently used"
+
+    field :errors, [Types::UserError, { null: false }], null: false, deprecation_reason: "Use attributeErrors or globalErrors"
 
     def attribute_errors
       object[:attribute_errors].map do |(path, type), messages|
