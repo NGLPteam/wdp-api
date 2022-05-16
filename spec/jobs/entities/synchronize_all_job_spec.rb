f3295756a854763c4a704564226004e2769f7074
Alexa Grey <alexa@castironcoding.com>
2022-05-16 00:30:51 -0700

    [C] Disable job uniqueness in tests

--- spec/jobs/entities/synchronize_all_job_spec.rb
+++ spec/jobs/entities/synchronize_all_job_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe Entities::SynchronizeAllJob, type: :job do
   it "enqueues a job for synchronizing communities" do
     expect do
       described_class.perform_now
-    end.to have_enqueued_job(Entities::SynchronizeCommunitiesJob).once
+    end.to execute_safely.and have_enqueued_job(Entities::SynchronizeCommunitiesJob).once
   end
 
   it "enqueues a job for synchronizing collections" do
     expect do
       described_class.perform_now
-    end.to have_enqueued_job(Entities::SynchronizeCollectionsJob).once
+    end.to execute_safely.and have_enqueued_job(Entities::SynchronizeCollectionsJob).once
   end
 
   it "enqueues a job for synchronizing entity links" do
     expect do
       described_class.perform_now
-    end.to have_enqueued_job(Entities::SynchronizeEntityLinksJob).once
+    end.to execute_safely.and have_enqueued_job(Entities::SynchronizeEntityLinksJob).once
   end
 
   it "enqueues a job for synchronizing items" do
     expect do
       described_class.perform_now
-    end.to have_enqueued_job(Entities::SynchronizeItemsJob).once
+    end.to execute_safely.and have_enqueued_job(Entities::SynchronizeItemsJob).once
   end
 end
