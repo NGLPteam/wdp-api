7df47207290c759bb964b63afa213c96a65d33b2
Alexa Grey <alexa@castironcoding.com>
2022-06-16 12:20:19 -0700

    [C] Use metadata format type for validation

--- app/validators/harvesting_metadata_format_validator.rb
+++ app/validators/harvesting_metadata_format_validator.rb
@@ -2,21 +2,12 @@
 
 # A validator for the metadata formats currently supported by WDP-API.
 class HarvestingMetadataFormatValidator < ActiveModel::EachValidator
-  # The currently supported metadata formats.
-  SUPPORTED_FORMATS = %w[
-    jats
-    mets
-    mods
-  ].freeze
-
-  private_constant :SUPPORTED_FORMATS
-
   # @param [ActiveModel::Validations, #errors] record
   # @param [Symbol] attribute
   # @param [String] value
   # @return [void]
   def validate_each(record, attribute, value)
-    return if value.in? SUPPORTED_FORMATS
+    return if Harvesting::Types::MetadataFormat.valid?(value)
 
     record.errors.add attribute, "must be a supported harvesting metadata format"
   end
