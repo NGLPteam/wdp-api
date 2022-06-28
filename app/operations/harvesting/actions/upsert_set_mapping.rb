516b2538480fdcc6242ee5575f8fde803f920434
Alexa Grey <alexa@castironcoding.com>
2022-06-28 14:57:08 -0700

    [C] Add ability to force a harvest set
    Some harvest sources do not actually enumerate their sets as they are
    supposed to, so we need the ability to force a set to be added to fit
    into the harvesting system.

--- app/operations/harvesting/actions/upsert_set_mapping.rb
+++ app/operations/harvesting/actions/upsert_set_mapping.rb
@@ -5,6 +5,7 @@ module Harvesting
     class UpsertSetMapping < Harvesting::BaseAction
       include Dry::Monads[:result, :do]
       include WDPAPI::Deps[
+        add_missing_set: "harvesting.sources.add_missing_set",
         extract_sets: "harvesting.actions.extract_sets",
         find_set: "harvesting.sources.find_set_by_identifier",
         upsert: "harvesting.mappings.upsert",
@@ -15,14 +16,17 @@ module Harvesting
         param :harvest_target, Harvesting::Types::Target
         param :set_identifier, Harvesting::Types::String
 
+        option :add_set_if_missing, Harvesting::Types::Bool, default: proc { false }
         option :read_options, Harvesting::Types::Hash.optional
       end
 
       # @param [HarvestSource] harvest_source
       # @param [HarvestTarget] harvest_target
       # @param [String] set_identifier
-      def perform(harvest_source, harvest_target, set_identifier, **options)
+      def perform(harvest_source, harvest_target, set_identifier, add_set_if_missing: false, **options)
         yield extract_sets.call harvest_source
+
+        yield add_missing_set.(harvest_source, set_identifier) if add_set_if_missing
 
         set = yield find_set.call(harvest_source, set_identifier)
 
