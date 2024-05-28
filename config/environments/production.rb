ba38cb68adf0c501e16fbc698ef230850f65a946
Alexa Grey <alexa@castironcoding.com>
2024-05-28 16:51:22 -0700

    [C] Add redis cache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,12 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, {
+    namespace: "rcache",
+    url: ENV["REDIS_URL"],
+    reconnect_attempts: 1,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
