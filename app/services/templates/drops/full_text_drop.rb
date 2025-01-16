77d24eca82d17128fca5adf47d74311f348fe9dd
Alexa Grey <alexa@castironcoding.com>
2025-01-16 13:31:05 -0800

    [E] Finesse markdown in templates

--- app/services/templates/drops/full_text_drop.rb
+++ app/services/templates/drops/full_text_drop.rb
@@ -8,6 +8,9 @@ module Templates
       # @return [String]
       attr_reader :content
 
+      # @return [FullText::Types::Kind]
+      attr_reader :kind
+
       # @param [Schemas::PropertyValues::FullText] value
       def initialize(value)
         super()
@@ -16,13 +19,25 @@ module Templates
 
         @value => { content:, kind:, lang:, }
 
-        @content = content
         @kind = kind
         @lang = lang
+
+        @content = prepare_content(content)
       end
 
       def to_s
         @content.presence
+      end
+
+      private
+
+      def prepare_content(raw_content)
+        case kind
+        in "markdown"
+          call_operation!("full_text.parse_markdown", raw_content)
+        else
+          raw_content
+        end
       end
     end
   end
