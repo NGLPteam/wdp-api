5c17943f6b4918f6aa8a795b88562926b69b2126
Alexa Grey <alexa@castironcoding.com>
2025-02-04 18:55:40 -0800

    [C] Speed up journal hierarchy context

--- spec/support/contexts/journal_hierarchy.rb
+++ spec/support/contexts/journal_hierarchy.rb
@@ -136,132 +136,102 @@ RSpec.shared_context "journal hierarchy" do
 
   include TestHelpers::JournalHierarchy::ExampleHelpers
 
-  before_all do
-    entities = []
+  let_it_be(:base_journal_community) do
+    Schemas::Orderings.with_disabled_refresh do
+      FactoryBot.create(:community, :with_logo, :with_thumbnail, identifier: "journal-community").tap(&:reload)
+    end
+  end
 
-    @journal_community = TestProf::AnyFixture.register(:journal_community) do
+  let_it_be(:base_journal) do
+    Schemas::Orderings.with_disabled_refresh do
+      FactoryBot.create(:collection, community: base_journal_community, schema: "nglp:journal").tap(&:reload)
+    end
+  end
+
+  jiter = TestHelpers::JournalHierarchy::JournalIterator.new
+
+  jiter.each do |viter|
+    viter => { index: vnum, identifier: vid }
+
+    let_it_be(:"base_#{vid}") do
+      identifier = vid
+      title = "Volume #{vnum}"
+
+      pending_properties = {
+        number: vnum.to_s,
+        sortable_number: vnum,
+      }
+
       Schemas::Orderings.with_disabled_refresh do
-        FactoryBot.create(:community, :with_logo, :with_thumbnail, identifier: "journal-community").tap(&:reload)
+        FactoryBot.create(:collection, schema: "nglp:journal_volume", community: base_journal_community, parent: base_journal, title:, identifier:, pending_properties:).tap(&:reload)
       end
     end
 
-    entities << @journal_community
+    viter.each do |iiter|
+      iiter => { index: inum, identifier: iid }
 
-    @journal = TestProf::AnyFixture.register(:journal) do
-      Schemas::Orderings.with_disabled_refresh do
-        FactoryBot.create(:collection, community: @journal_community, schema: "nglp:journal").tap(&:reload)
-      end
-    end
-
-    entities << @journal
-
-    jiter = TestHelpers::JournalHierarchy::JournalIterator.new
-
-    jiter.each do |viter|
-      viter => { index: vnum, identifier: vid }
-
-      volume = TestProf::AnyFixture.register(vid) do
-        identifier = vid
-        title = "Volume #{vnum}"
+      let_it_be(:"base_#{iid}") do
+        identifier = iid
+        title = "Issue #{inum}"
 
         pending_properties = {
-          number: vnum.to_s,
-          sortable_number: vnum,
+          number: inum.to_s,
+          sortable_number: inum.to_s,
+          volume: {
+            sortable_number: vnum,
+          },
         }
 
         Schemas::Orderings.with_disabled_refresh do
-          FactoryBot.create(:collection, schema: "nglp:journal_volume", community: @journal_community, parent: @journal, title:, identifier:, pending_properties:).tap(&:reload)
+          FactoryBot.create(:collection, schema: "nglp:journal_issue", community: base_journal_community, parent: __send__(:"base_#{vid}"), title:, identifier:, pending_properties:).tap(&:reload)
         end
       end
 
-      entities << volume
+      iiter.each do |aiter|
+        aiter => { index: anum, identifier: aid }
 
-      viter.each do |iiter|
-        iiter => { index: inum, identifier: iid }
+        let_it_be(:"base_#{aid}") do
+          identifier = aid.to_s
 
-        issue = TestProf::AnyFixture.register(iid) do
-          identifier = iid
-          title = "Issue #{inum}"
+          title = "Article #{anum}"
 
           pending_properties = {
-            number: inum.to_s,
-            sortable_number: inum.to_s,
+            abstract: Faker::Lorem.paragraph,
+            citation: Faker::Lorem.paragraph,
+            preprint_version: false,
             volume: {
               sortable_number: vnum,
             },
+            issue: {
+              sortable_number: inum,
+            },
+            meta: {
+              collected: VariablePrecisionDate.parse(2022),
+              page_count: 2,
+            }
           }
 
           Schemas::Orderings.with_disabled_refresh do
-            FactoryBot.create(:collection, schema: "nglp:journal_issue", community: @journal_community, parent: volume, title:, identifier:, pending_properties:).tap(&:reload)
+            FactoryBot.create(:item, schema: "nglp:journal_article", collection: __send__(:"base_#{iid}"), title:, identifier:, pending_properties:).tap(&:reload)
           end
-        end
-
-        entities << issue
-
-        iiter.each do |aiter|
-          aiter => { index: anum, identifier: aid }
-
-          article = TestProf::AnyFixture.register(aid) do
-            identifier = aid.to_s
-
-            title = "Article #{anum}"
-
-            pending_properties = {
-              abstract: Faker::Lorem.paragraph,
-              citation: Faker::Lorem.paragraph,
-              preprint_version: false,
-              volume: {
-                sortable_number: vnum,
-              },
-              issue: {
-                sortable_number: inum,
-              },
-              meta: {
-                collected: VariablePrecisionDate.parse(2022),
-                page_count: 2,
-              }
-            }
-
-            Schemas::Orderings.with_disabled_refresh do
-              FactoryBot.create(:item, schema: "nglp:journal_article", collection: issue, title:, identifier:, pending_properties:).tap(&:reload)
-            end
-          end
-
-          entities << article
         end
       end
     end
-
-    entities.each do |entity|
-      refresh_journal_fixture(entity)
-    end
   end
 
-  let_it_be(:journal_community, refind: true) { fixture(:journal_community) }
-  let_it_be(:journal, refind: true) { fixture(:journal) }
+  let_it_be(:journal_community, refind: true) { refresh_journal_fixture(base_journal_community) }
+  let_it_be(:journal, refind: true) { refresh_journal_fixture(base_journal) }
 
   TestHelpers::JournalHierarchy::JournalIterator.all_identifiers.each do |identifier|
-    let_it_be(identifier, refind: true) { fixture(identifier) }
+    let_it_be(identifier, refind: true) { refresh_journal_fixture(__send__(:"base_#{identifier}")) }
   end
-end
 
-before_fixtures_reset do
-  comm = Community.find_by(identifier: "journal-community")
-
-  next if comm.blank?
-
-  comm.collections.filtered_by_schema_version("nglp:journal").find_each do |jrn|
-    jrn.descendants.filtered_by_schema_version("nglp:journal_volume").find_each do |vol|
-      vol.descendants.filtered_by_schema_version("nglp:journal_issue").find_each do |iss|
-        iss.items.find_each(&:destroy!)
-
-        iss.destroy!
-      end
-
-      vol.destroy!
+  let_it_be(:journal_entities) do
+    [
+      journal_community,
+      journal,
+    ] + TestHelpers::JournalHierarchy::JournalIterator.all_identifiers.map do |identifier|
+      __send__(identifier)
     end
-    jrn.destroy!
   end
-
-  comm.destroy!
 end
