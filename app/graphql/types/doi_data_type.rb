be46908ed7df6339c468080adfc867a5679c79e2
Alexa Grey <alexa@castironcoding.com>
2025-02-06 16:32:10 -0800

    [B] Actually expose DOI data

--- app/graphql/types/doi_data_type.rb
+++ app/graphql/types/doi_data_type.rb
@@ -2,5 +2,32 @@
 
 module Types
   class DOIDataType < Types::BaseObject
+    description <<~TEXT
+    Validated and sanitized DOI information for an entity.
+    TEXT
+
+    field :doi, String, null: true do
+      description <<~TEXT
+      A pristine, validated DOI value, absent any URL information.
+      TEXT
+    end
+
+    field :host, String, null: true do
+      description <<~TEXT
+      The host that this DOI uses. Will be `"doi.org"` in most cases.
+      TEXT
+    end
+
+    field :ok, Boolean, null: false do
+      description <<~TEXT
+      Whether the DOI is ok and valid.
+      TEXT
+    end
+
+    field :url, String, null: true do
+      description <<~TEXT
+      The full URL for the DOI.
+      TEXT
+    end
   end
 end
