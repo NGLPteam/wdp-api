42d0f50c1dae05fdd73bcdaa3bc335105ee1c0ed
Alexa Grey <alexa@castironcoding.com>
2025-03-14 12:53:27 -0700

    [C] Simplfy asset detection
    There is an issue with some PDF assets errantly being detected as
    documents until their metadata is manually refreshed. This will solve
    the issue by no longer backgrounding promotion, so that the actual
    file can be used for kind detection, as well as being removing delays
    around assets being readily available for the templating system.

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,16 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Asset, type: :model do
-  include ActiveJob::TestHelper
-
   context "with processed attachments" do
     let!(:asset_trait) { :image }
     let!(:asset) do
-      perform_enqueued_jobs do
-        FactoryBot.create :asset, asset_trait
-      end.then do |asset|
-        asset.reload
-      end
+      FactoryBot.create :asset, asset_trait
     end
 
     subject { asset }
@@ -47,10 +41,8 @@ RSpec.describe Asset, type: :model do
 
     it "can destroy an asset properly" do
       expect do
-        perform_enqueued_jobs do
-          asset.destroy
-        end
-      end.to execute_safely.and change(described_class, :count).by(-1).and have_performed_job(Processing::DestroyAttachmentJob).once
+        asset.destroy
+      end.to execute_safely.and change(described_class, :count).by(-1)
     end
   end
 end
