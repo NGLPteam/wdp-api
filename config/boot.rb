74bbe22a35aa27c0371706dc39c3ac1dc45edbf7
Alexa Grey <alexa@castironcoding.com>
2025-03-14 11:05:32 -0700

    [C] More niso-jats workarounds

--- config/boot.rb
+++ config/boot.rb
@@ -7,5 +7,11 @@ Warning[:experimental] = false # Hush.
 # Annoying workaround for niso-jats gem in production.
 module NisoJats; end
 
+module Niso
+  module Jats
+    Version = "0.1.1"
+  end
+end
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
