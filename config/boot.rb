c7514e69a19510766e8122c629566a40ef65a54b
Alexa Grey <alexa@castironcoding.com>
2025-03-14 10:54:39 -0700

    [C] Potentially address production issue with niso-jats gem

--- config/boot.rb
+++ config/boot.rb
@@ -4,5 +4,8 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 Warning[:experimental] = false # Hush.
 
+# Annoying workaround for niso-jats gem in production.
+module NisoJats; end
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
